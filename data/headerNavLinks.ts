fb57dad352a834fa9efb68bcedb5b8eac833538a
hyzm <3535377512@qq.com>
2024-09-11 00:36:54 +0800

    refactor: change meta data

--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -1,9 +1,9 @@
 const headerNavLinks = [
-  { href: '/', title: 'Home' },
-  { href: '/blog', title: 'Blog' },
-  { href: '/tags', title: 'Tags' },
-  { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/', title: '首页' },
+  { href: '/blog', title: '文章' },
+  { href: '/tags', title: '标签' },
+  // { href: '/projects', title: 'Projects' },
+  // { href: '/about', title: 'About' },
 ]
 
 export default headerNavLinks
